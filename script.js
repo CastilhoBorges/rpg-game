746253dc84676b9ff529532b566622f2cae8d2b5
CastilhoBorges <augustocastilhoborges21@gmail.com>
2024-09-11 16:21:06 -0300

    Estilos principais e logica para ir para loja e voltar

--- script.js
+++ script.js
@@ -4,6 +4,14 @@ const button1 = document.querySelector("#button1");
 const button2 = document.querySelector("#button2");
 const button3 = document.querySelector("#button3");
 
+const text = document.querySelector("#text");
+const xpText = document.querySelector("#xpText");
+const healthText = document.querySelector("#healthText");
+const goldText = document.querySelector("#goldText");
+const monsterStats = document.querySelector("#monsterStats");
+const monsterName = document.querySelector("#monsterName");
+const monsterHealthText = document.querySelector("#monsterHealth");
+
 const inventory = new Array("stick");
 
 let xp = 0;
@@ -11,3 +19,49 @@ let health = 100;
 let gold = 50;
 let currentWeaponIndex = 0;
 let fighting;
+
+const locations = [
+  {
+    name: "town square",
+    "button text": ["Go to store", "Go to cave", "Fight dragon"],
+  },
+];
+
+const update = function (location) {};
+
+const goTown = function () {
+  text.innerText = `You are in the town square. You see a sign that says "Store".`;
+
+  button1.innerText = "Go to store";
+  button2.innerText = "Go to cave";
+  button3.innerText = "Fight dragon";
+
+  button1.onclick = goStore;
+  button2.onclick = goCave;
+  button3.onclick = fightDragon;
+};
+
+const buyHealth = function () {};
+
+const buyWeapon = function () {};
+
+const goStore = function () {
+  text.innerText = "You enter the store.";
+
+  button1.innerText = "Buy 10 health (10 gold)";
+  button2.innerText = "Buy weapon (30 gold)";
+  button3.innerText = "Go to town square";
+
+  button1.onclick = buyHealth;
+  button2.onclick = buyWeapon;
+  button3.onclick = goTown;
+};
+
+const goCave = function () {};
+
+const fightDragon = function () {};
+
+// initialize buttons
+button1.onclick = goStore;
+button2.onclick = goCave;
+button3.onclick = fightDragon;
